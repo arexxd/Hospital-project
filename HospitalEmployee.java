556c504fd44a06928478536ed480c0855374de50
arexxd <60717897+arexxd@users.noreply.github.com>
2022-02-24 15:29:17 -0600

    Created abstract class

---
+++ HospitalEmployee.java
@@ -0,0 +1,24 @@
+public abstract class HospitalEmployee {
+    private String name, id;
+    
+    public HospitalEmployee(String employeename, String  employeeid)
+        name = employeename;
+        id = employeeid;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public String getid()
+    {
+        return id;
+    }
+
+    public String toString()
+    {
+       return "Employee" + name + "Employee ID" + id; 
+    }
+abstract public String
+}
