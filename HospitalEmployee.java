cafccf51c53198d2081a0687854608cbfaa848d0
arexxd <60717897+arexxd@users.noreply.github.com>
2022-02-27 22:24:39 -0600

    Update HospitalEmployee.java

--- HospitalEmployee.java
+++ HospitalEmployee.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.NameList;
+
 public abstract class HospitalEmployee {
     private String name, id;
     
@@ -29,8 +31,66 @@ abstract public String work();
 abstract public String quote();
 }
 class Doctor extends HospitalEmployee{
-    private String name, id;
+    private String name, id;     
     public Doctor(String name, String id){
-
+        super(name,id);
+        this.name = name;
+        this.id = id;
+    }
+    public String work()
+    {
+        return "I am a "+id;
+    }
+    public String quote()
+    {
+        return "\"I'm not superstitious, but I am a little stitious\" -Steve Carrell";
+    }  
+}
+class Nurse extends HospitalEmployee{
+    private String name, id;     
+    public Nurse(String name, String id){
+        super(name,id);
+        this.name = name;
+        this.id = id;
+    }
+    public String work()
+    {
+        return "I am a "+id;
+    }
+    public String quote()
+    {
+        return "\"Why do they call it rush hour when nothing moves?\" -Robin Williams";
+    }  
+}
+class Administrator extends HospitalEmployee{
+    private String name, id;     
+    public Administrator(String name, String id){
+        super(name,id);
+        this.name = name;
+        this.id = id;
+    }
+    public String work()
+    {
+        return "I am a "+id;
+    }
+    public String quote()
+    {
+        return "\"Before you marry a person make them use a computer with slow internet to see who they really are\" -Will Ferell";
+    }  
+}
+class Janitor extends Administrator{
+    private String name, id;
+    public Janitor(String name, String id){
+        super(name,id);
+        this.name = name;
+        this.id = id;
+    }
+    public String work()
+    {
+        return "I am a "+id;
+    }
+    public String quote()
+    {
+        return "\"People say nothing is impossible but I do nothing every day.\"";
     }
 }
